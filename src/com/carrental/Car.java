6fa9771b153243e3bf352e8d333f3e3bca2b36ce
PrinceDev <pri1999.m.pm@gmail.com>
2025-07-29 23:01:19 +0530

    updated classes

---
+++ src/com/carrental/Car.java
@@ -0,0 +1,19 @@
+package com.carrental;
+
+public class Car {
+
+    private int carId;
+    private String carBrand;
+    private String carModel;
+    private double carBasePricePerDay;
+
+    Car(int carId, String carBrand, String carModel){
+
+    }
+
+
+
+
+
+
+}
